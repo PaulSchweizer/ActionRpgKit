f02d69171d4d9b809cee43f9490a8dfad37a1be1
Paul Schweizer <paulschweizer@gmx.net>
2016-08-25 08:57:36 -0700

    Create GameTime.cs

---
+++ Core/GameTime.cs
@@ -0,0 +1,16 @@
+public static class GameTime
+{
+    private static float _fixedTime; 
+
+    public static float time
+    {
+        get
+        {
+            return _fixedTime;
+        }
+        set
+        {
+            _fixedTime = value;
+        }
+    }
+}
