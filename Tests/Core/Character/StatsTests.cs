08fdd0cdb080c64812afd1a0d9cfec74f543a1f2
Paul Schweizer <paulschweizer@gmx.net>
2016-08-25 10:10:38 -0700

    Update StatsTests.cs

--- Tests/Core/Character/StatsTests.cs
+++ Tests/Core/Character/StatsTests.cs
@@ -8,19 +8,11 @@ namespace CharacterTests
     [Category("Character.Stats")]
     public class CharacterStatsTests
     {
-        Character.EnemyStats enemyStats;
 
         [SetUp]
         public void SetUp ()                                                  
         {
-            enemyStats = new Character.EnemyStats(10, 20, 30);
             GameTime.time = 0f;
-        }
-
-        [Test]
-        public void PrimaryAttributeTest()
-        {
-
         }
     }
 }
