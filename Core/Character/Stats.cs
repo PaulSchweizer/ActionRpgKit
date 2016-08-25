f9a9e71986d9396e952d407b79a0f38d709d2f2f
Paul Schweizer <paulschweizer@gmx.net>
2016-08-25 10:05:14 -0700

    Update Stats.cs

--- Core/Character/Stats.cs
+++ Core/Character/Stats.cs
@@ -19,35 +19,4 @@ namespace Character
         IAttribute Life {get; set; }
         IAttribute Magic {get; set; }
     }
-
-    /// <summary>
-    /// These Stats describe an Enemy Character.
-    /// </summary>
-    public class EnemyStats : IStats
-    {
-        // Primary Attributes
-        public Attribute Body;
-        public Attribute Mind;
-        public Attribute Soul;
-        public Attribute Experience;
-
-        // Secondary Attributes
-        public IAttribute Level;
-        public IAttribute Life;
-        public IAttribute Magic;
-
-        /// <summary>
-        /// Initialize the Stats with default primary attribute values.
-        /// </summary>
-        public EnemyStats (float body = 0, float mind = 0, float soul = 0)
-        {
-            // Primary Attributes
-            Body = new Attribute("Body", 0, 999, body);
-            Mind = new Attribute("Mind", 0, 999, mind);
-            Soul = new Attribute("Soul", 0, 999, soul);
-            Experience = new Attribute("Experience");
-
-            // Secondary Attributes
-        }
-    }
 }
