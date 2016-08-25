33011080d7f1f55dbf1d2dfcb7f67dd3fd5d8407
Paul Schweizer <paulschweizer@gmx.net>
2016-08-25 09:51:06 -0700

    Update AttributeTests.cs

--- Tests/Core/Character/AttributeTests.cs
+++ Tests/Core/Character/AttributeTests.cs
@@ -1,1 +1,24 @@
 
+using System;
+using NUnit.Framework;
+namespace CharacterTests
+{
+    [TestFixture]
+    [Category("Character.Attribute")]
+    public class CharacterAttributeTests
+    {
+        Character.PrimaryAttribute primaryAttribute;
+        
+        [SetUp]
+        public void SetUp ()
+        {
+            primaryAttribute = new Character.PrimaryAttribute();
+            GameTime.time = 0f;
+        }
+        
+        [Test]
+        public void PrimaryAttributeTest()
+        {
+        }
+    }
+}
