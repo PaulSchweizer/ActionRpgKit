fe0bbfa5411d72b6af147ce69eba38c2fcfab8d8
Paul Schweizer <paulschweizer@gmx.net>
2016-08-26 11:27:36 -0700

    Update AttributeTests.cs

--- Tests/Core/Character/AttributeTests.cs
+++ Tests/Core/Character/AttributeTests.cs
@@ -19,6 +19,13 @@ namespace CharacterTests
         [Test]
         public void PrimaryAttributeTest()
         {
+            // Set some values
+            primaryAttribute.Value = 12;
+            Assert.AreEqual(primaryAttribute.Value, 12);     
+            primaryAttribute.Value = 10000;
+            Assert.AreEqual(primaryAttribute.Value, 999);
+            primaryAttribute.Value = -10000;
+            Assert.AreEqual(primaryAttribute.Value, 0);
         }
     }
 }
