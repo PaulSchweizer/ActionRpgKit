97f1682fe0ef24ac260039d2ccaedb6435128b36
Paul Schweizer <paulschweizer@gmx.net>
2016-08-26 17:22:14 -0700

    Update Attribute.cs

--- Core/Character/Attribute.cs
+++ Core/Character/Attribute.cs
@@ -220,42 +220,42 @@ namespace Character.Attribute
      public class VolumeAttribute : SecondaryAttribute
     {
 
-        private float _currentValue;
-        private float _absoluteMaxValue;
+        // private float _currentValue;
+        // private float _absoluteMaxValue;
 
-        public VolumeAttribute (string name, 
-                                Formula formula, 
-                                IAttribute[] attributes,
-                                float minValue = float.MinValue,
-                                float maxValue = float.MaxValue) : 
-                                base(name, formula, attributes, minValue, maxValue)
-        {
-            _currentValue = MaxValue;
-        }
+        // public VolumeAttribute (string name, 
+        //                         Formula formula, 
+        //                         IAttribute[] attributes,
+        //                         float minValue = float.MinValue,
+        //                         float maxValue = float.MaxValue) : 
+        //                         base(name, formula, attributes, minValue, maxValue)
+        // {
+        //     _currentValue = MaxValue;
+        // }
         
-        public override float Value
-        {
-            get
-            {
-                return _currentValue;
-            }
-            set
-            {
-                _currentValue = Math.Max(MinValue, Math.Min(MaxValue, value));
-            }
-        }
+        // public override float Value
+        // {
+        //     get
+        //     {
+        //         return _currentValue;
+        //     }
+        //     set
+        //     {
+        //         _currentValue = Math.Max(MinValue, Math.Min(MaxValue, value));
+        //     }
+        // }
 
-        public override float MaxValue
-        {
-            get
-            {
-                return Math.Min(_absoluteMaxValue, base.BaseValue);
-            }
-            set 
-            {
-                _absoluteMaxValue = value;
-            }
-        }
+        // public override float MaxValue
+        // {
+        //     get
+        //     {
+        //         return Math.Min(_absoluteMaxValue, base.BaseValue);
+        //     }
+        //     set 
+        //     {
+        //         _absoluteMaxValue = value;
+        //     }
+        // }
     }
  
     public class Modifier 
