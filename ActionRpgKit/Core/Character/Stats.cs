5622942cf0fbf1eef44f562cf6766a759350621f
Paul Schweizer <paulschweizer@gmx.net>
2016-08-29 11:55:58 -0700

    Update Stats.cs

--- ActionRpgKit/Core/Character/Stats.cs
+++ ActionRpgKit/Core/Character/Stats.cs
@@ -1,7 +1,66 @@
+using Character.Attribute;
+
 namespace Character.Stats 
 {
     public interface IStats
     {
+        IAttribute Magic { get; set; }
+    }
     
+    public class PlayerStats
+    {
+        public IAttribute Soul;
+        public IAttribute Experience;
+        public IAttribute Level;
+        private IAttribute _magic;
+       
+        public PlayerStats ()
+        {
+            // Primary Attributes
+            Soul = new BaseAttribute("Soul", 0, 999, 0);
+            Experience = new Attribute("Experience");
+
+            // Secondary Attributes
+            Level = new SecondaryAttribute("Level",
+                x => (int)(Math.Sqrt(x[0].Value / 100)) * 1f, 
+                new IAttribute[] { Experience }, 0, 99);
+            Magic = new VolumeAttribute("Magic", 
+                x => (int)(20 + 5 * x[0].Value + x[1].Value / 3) * 1f, 
+                new IAttribute[] { Level, Soul }, 0, 999);
+        }
+        
+        public IAttribute Magic 
+        {
+            get 
+            {
+                return _magic;
+            }
+            set
+            {
+                _magic = value;
+            }
+        }
+    }
+    
+    public class EnemyStats
+    {
+        private IAttribute _magic;
+        
+        public EnemyStats ()
+        {
+            Magic = new BaseAttribute("Magic", 0, 999, 0);
+        }
+        
+        public IAttribute Magic 
+        {
+            get 
+            {
+                return _magic;
+            }
+            set
+            {
+                _magic = value;
+            }
+        }
     }
 }
