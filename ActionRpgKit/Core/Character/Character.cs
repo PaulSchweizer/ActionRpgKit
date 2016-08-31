2706762a69d95cab572d03aaff47862cc3cfdc9d
Paul Schweizer <paulschweizer@gmx.net>
2016-08-31 15:37:33 -0700

    Update Character.cs

--- ActionRpgKit/Core/Character/Character.cs
+++ ActionRpgKit/Core/Character/Character.cs
@@ -5,15 +5,35 @@ using Character.Stats;
 
 namespace Character
 {
+    /// <summary>
+    /// Characters populate the game world. 
+    /// They are defined by Stats and can make use of their 
+    /// learned Skills.</summary>
     public interface ICharacter
     {
+        /// <summary>
+        /// Name of the character.</summary>
         string Name { get; }
-        BaseStats Stats { get; set;}
+        
+        /// <summary>
+        /// Stats describing the Character.</summary>
+        BaseStats Stats { get; set; }
+
+        /// <summary>
+        /// Skills available for this Character.</summary>
         List<ISkill> Skills { get; }
+
+        /// <summary>
+        /// Add a new Skill to the list of available Skills.</summary>
         void LearnSkill (ISkill skill);
+
+        /// <summary>
+        /// Trigger the given Skill.</summary>
         bool TriggerSkill (ISkill skill);
     }
-
+    
+    /// <summary>
+    /// Base implementation of a Character.</summary>
     public class BaseCharacter : ICharacter
     {
         private string _name;
@@ -54,6 +74,11 @@ namespace Character
             }
         }
 
+        /// <summary>
+        /// Check if the character knows the skill, has enough 
+        /// magic energy and is not in cooldown of the Skill.</summary>
+        /// <param name=skill>The Skill to test</param>
+        /// <returns> Whether the Skill van be used.</returns>
         private bool SkillCanBeUsed(ISkill skill)
         {
             if (!Skills.Contains(skill))
@@ -85,6 +110,10 @@ namespace Character
             return true;
         }
 
+        /// <summary>
+        /// A countdown before the Skill takes action.</summary>
+        /// <remarks>This can be used for syncing animations or effects.</remarks>
+        /// <param name=skill>The Skill to use</param>
         public virtual void PreUseCountdown(ISkill skill)
         {
             //
@@ -92,13 +121,18 @@ namespace Character
             //
             UseSkill(skill);
         }
-
+        
+        /// <summary>
+        /// Triggers the use of the Skill</summary>
+        /// <param name=skill>The Skill to use</param>
         private void UseSkill(ISkill skill)
         {
             skill.Use(this);
         }
     }
-
+    
+    /// <summary>
+    /// Representation of a Player controllable character.</summary>
     public class Player : BaseCharacter
     {
         public Player(string name) : base(name)
@@ -106,7 +140,9 @@ namespace Character
             Stats = new PlayerStats();
         }
     }
-
+    
+    /// <summary>
+    /// Representation of a Hostile, game controlled character.</summary>
     public class Enemy : BaseCharacter
     {
         public Enemy(string name) : base(name)
