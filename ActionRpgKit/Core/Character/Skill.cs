5c41899a2870591af1e8393fdcfcfd6c24056ba2
Paul Schweizer <paulschweizer@gmx.net>
2016-09-02 09:36:59 -0700

    Update Skill.cs

--- ActionRpgKit/Core/Character/Skill.cs
+++ ActionRpgKit/Core/Character/Skill.cs
@@ -5,6 +5,10 @@ using Character.Attribute;
 
 namespace Character.Skill
 {
+    // ------------------------------------------------------------------------
+    // Interfaces
+    // ------------------------------------------------------------------------
+
     /// <summary>
     /// Interface for Skills.</summary>
     public interface ISkill
@@ -13,30 +17,26 @@ namespace Character.Skill
         /// Unique identifier for the Skill. </summary>
         /// <remarks>This can be used to create a database of Skills.</remarks>
         int Id { get; }
-        
+
         /// <summary>
-        /// Name of the Skill.</summary>  
+        /// Name of the Skill.</summary>
         string Name { get; }
-        
+
         /// <summary>
-        /// A description.</summary>          
+        /// A description.</summary>
         string Description { get; }
-        
+
         /// <summary>
-        /// The time prior to the usage.</summary>  
+        /// The time prior to the usage.</summary>
         float PreUseTime { get; }
-        
+
         /// <summary>
-        /// The time it takes until the Skill can be used again.</summary>  
+        /// The time it takes until the Skill can be used again.</summary>
         float CooldownTime { get; }
 
         /// <summary>
-        /// Whether the skill matches the given series of items.</summary>  
-        bool Match();
-        
-        /// <summary>
-        /// The Skill is used and takes effect.</summary>  
-        void Use(ICharacter user);
+        /// Whether the skill matches the given series of items.</summary>
+        bool Match ();
     }
 
     /// <summary>
@@ -44,30 +44,111 @@ namespace Character.Skill
     public interface IMagicSkill : ISkill
     {
         /// <summary>
-        /// The energy costs of the Skill.</summary>  
+        /// The energy costs of the Skill.</summary>
         float Cost { get; }
+
+        /// <summary>
+        /// The Skill is used and takes effect.</summary>
+        void Use (ICharacter user);
     }
-    
+
     /// <summary>
     /// A Skill to be used as an Attack in Combat.</summary>
     public interface ICombatSkill : ISkill
     {
         /// <summary>
-        /// The amount of damage.</summary>  
+        /// The amount of damage.</summary>
         float Damage { get; }
+
+        /// <summary>
+        /// The maximum amount of enemies that can be targeted at once.</summary>
+        int MaximumTargets { get; }
+
+        /// <summary>
+        /// The Skill is used and takes effect.</summary>
+        void Use (IFighter user);
     }
 
+    // ------------------------------------------------------------------------
+    // Abstracts
+    // ------------------------------------------------------------------------
+
     /// <summary>
-    /// A passive Skill acts on the User itself.</summary>
-    public class PassiveMagicSkill : IMagicSkill
+    /// A basic Skill implementation.</summary>
+    public abstract class BaseSkill : ISkill
     {
         private int _id;
         private string _name;
         private string _description;
-        private float _cost;
-        private float _duration;
         private float _preUseTime;
         private float _cooldownTime;
+
+        public BaseSkill(int id,
+                         string name,
+                         string description,
+                         float preUseTime,
+                         float cooldownTime)
+        {
+            _id = id;
+            _name = name;
+            _description = description;
+            _preUseTime = preUseTime;
+            _cooldownTime = cooldownTime;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+        }
+
+        public float CooldownTime
+        {
+            get
+            {
+                return _cooldownTime;
+            }
+        }
+
+        public float PreUseTime
+        {
+            get
+            {
+                return _preUseTime;
+            }
+        }
+
+        public abstract bool Match();
+    }
+
+    // ------------------------------------------------------------------------
+    // Implementations
+    // ------------------------------------------------------------------------
+
+    /// <summary>
+    /// A passive MagicSkill adds buffs on the User itself.</summary>
+    public class PassiveMagicSkill : BaseSkill, IMagicSkill
+    {
+        private float _cost;
+        private float _duration;
         private float _endTime = -1;
         private float _modifierValue;
         private string _modifiedAttributeName;
@@ -80,42 +161,30 @@ namespace Character.Skill
                                  float preUseTime,
                                  float cooldownTime,
                                  float modifierValue,
-                                 string modifiedAttributeName)
+                                 string modifiedAttributeName) : base(id,
+                                                                      name,
+                                                                      description,
+                                                                      preUseTime,
+                                                                      cooldownTime)
         {
-            _id = id;
-            _name = name;
-            _description = description;
             _cost = cost;
             _duration = duration;
-            _preUseTime = preUseTime;
-            _cooldownTime = cooldownTime;
             _modifierValue = modifierValue;
             _modifiedAttributeName = modifiedAttributeName;
         }
 
-        public int Id
+        // --------------------------------------------------------------------
+        // ISkill implementations
+        // --------------------------------------------------------------------
+
+        public override bool Match()
         {
-            get
-            {
-                return _id;
-            }
+            throw new NotImplementedException();
         }
 
-        public string Name
-        {
-            get
-            {
-                return _name;
-            }
-        }
-
-        public string Description
-        {
-            get
-            {
-                return _description;
-            }
-        }
+        // --------------------------------------------------------------------
+        // IMagicSkill implementations
+        // --------------------------------------------------------------------
 
         public float Cost
         {
@@ -126,123 +195,81 @@ namespace Character.Skill
         }
 
         /// <summary>
-        /// A new TimeBasedModifier is returned everytime it is requested.</summary>  
-        private IModifier GetModifier ()
-        { 
-            return new TimeBasedModifier(Name, _modifierValue, _duration);
-        }
-
-        public float CooldownTime
-        {
-            get
-            {
-                return _cooldownTime;
-            }
-        }
-
-        public float PreUseTime
-        {
-            get
-            {
-                return _preUseTime;
-            }
-        }
-
-        public bool Match()
-        {
-            throw new NotImplementedException();
-        }
-        
-        /// <summary>
-        /// Add the modifier to the modified attribute.</summary>  
-        public void Use(ICharacter user)
+        /// Add the modifier to the modified attribute.</summary>
+        public void Use (ICharacter user)
         {
             user.Stats[_modifiedAttributeName].AddModifier(GetModifier());
         }
+
+        /// <summary>
+        /// A new TimeBasedModifier is returned everytime it is requested.</summary>
+        private IModifier GetModifier ()
+        {
+            return new TimeBasedModifier(Name, _modifierValue, _duration);
+        }
     }
-    
-    public class MeleeSkill : ICombatSkill
+
+    /// <summary>
+    /// Allows to attack with a melee weapon.</summary>
+    public class MeleeSkill : BaseSkill, ICombatSkill
     {
-        private int _id;
-        private string _name;
-        private string _description;
-        private float _preUseTime;
-        private float _cooldownTime;
+
         private float _damage;
+        private int _maximumTargets;
 
         public MeleeSkill(int id,
                           string name,
                           string description,
                           float preUseTime,
                           float cooldownTime,
-                          float damage)
+                          float damage,
+                          int maximumTargets) : base(id,
+                                                     name,
+                                                     description,
+                                                     preUseTime,
+                                                     cooldownTime)
         {
-            _id = id;
-            _name = name;
-            _description = description;
-            _preUseTime = preUseTime;
-            _cooldownTime = cooldownTime;
             _damage = damage;
-        }
-        
-        public int Id
-        {
-            get
-            {
-                return _id;
-            }
+            _maximumTargets = maximumTargets;
         }
 
-        public string Name
+        // --------------------------------------------------------------------
+        // ISkill implementations
+        // --------------------------------------------------------------------
+
+        public override bool Match ()
         {
-            get
-            {
-                return _name;
-            }
+            throw new NotImplementedException();
         }
 
-        public string Description
-        {
-            get
-            {
-                return _description;
-            }
-        }
-        
-        public float CooldownTime
-        {
-            get
-            {
-                return _cooldownTime;
-            }
-        }
+        // --------------------------------------------------------------------
+        // ICombatSkill implementations
+        // --------------------------------------------------------------------
 
-        public float PreUseTime
+        public float Damage
         {
-            get
-            {
-                return _preUseTime;
-            }
-        }
-        
-        public float Damage 
-        { 
             get
             {
                 return _damage;
             }
         }
 
-        public bool Match()
+        public int MaximumTargets
         {
-            throw new NotImplementedException();
+            get
+            {
+                return _maximumTargets;
+            }
         }
-        
+
         /// <summary>
-        /// Add the modifier to the modified attribute.</summary>  
-        public void Use(ICharacter user)
+        /// Inform the attacked Characters that they are being attacked.</summary>
+        public void Use (IFighter user)
         {
-            throw new NotImplementedException();
+            for (int i = Math.Min(MaximumTargets, user.Enemies.Count) - 1; i >= 0; i--)
+            {
+                user.Enemies[i].OnAttacked(user, Damage);
+            }
         }
     }
 }
