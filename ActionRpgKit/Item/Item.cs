4ef8cb494fbbd80da8166f5dd8a289d2b903e63d
Paul Schweizer <paulschweizer@gmx.net>
2016-09-14 12:23:48 -0700

    Update Item.cs

--- ActionRpgKit/Item/Item.cs
+++ ActionRpgKit/Item/Item.cs
@@ -2,13 +2,26 @@
 
 namespace ActionRpgKit.Item
 {
+    /// <summary>
+    /// An Item is something that is usable by Characters.</summary>
     public interface IItem
     {
+        /// <summary>
+        /// The id is used to identify the Item and possibly link it to 
+        /// a database of any kind.</summary>
         int Id { get; set; }
+        
+        /// <summary>
+        /// A preferrably unique name.</summary>
         string Name { get; set; }
+        
+        /// <summary>
+        /// The description can be shown in the Game to provide further information.</summary>
         string Description{ get; set; }
     }
 
+    /// <summary>
+    /// Basic implementation of the IItem.</summary>
     public abstract class BaseItem : IItem
     {
         public int Id
@@ -27,6 +40,8 @@ namespace ActionRpgKit.Item
         }
     }
 
+    /// <summary>
+    /// This item is usable for Skills.</summary>
     public class UsableItem : BaseItem
     {
 
