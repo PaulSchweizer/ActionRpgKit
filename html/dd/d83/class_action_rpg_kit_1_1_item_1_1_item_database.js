d9da90f141631bab0e9d1c110ba570da89cdb28e
Travis CI <travis@travis-ci.org>
2016-09-16 23:39:27 +0000

    Deploy code docs to GitHub Pages Travis build: 306
    Commit: 5150fe49e42516d111d7062ebfde2a2727e48c6d

--- html/dd/d83/class_action_rpg_kit_1_1_item_1_1_item_database.js
+++ html/dd/d83/class_action_rpg_kit_1_1_item_1_1_item_database.js
@@ -1,5 +1,6 @@
 var class_action_rpg_kit_1_1_item_1_1_item_database =
 [
     [ "GetItemByName", "dd/d83/class_action_rpg_kit_1_1_item_1_1_item_database.xhtml#ae500b4747c85689ee6004d5d9bc04a2d", null ],
+    [ "GetItemById", "dd/d83/class_action_rpg_kit_1_1_item_1_1_item_database.xhtml#af2ad9a07df33760dd27f2a44f3d1bf43", null ],
     [ "Items", "dd/d83/class_action_rpg_kit_1_1_item_1_1_item_database.xhtml#a417f39bebf75cf0a0c3d256097f6d123", null ]
 ];
--- html/search/functions_67.js
+++ html/search/functions_67.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['gamestoryline',['GameStoryline',['../d4/d25/class_action_rpg_kit_1_1_tests_1_1_game_storyline.xhtml#ae727b573d523dd5d11afd002ebb81505',1,'ActionRpgKit::Tests::GameStoryline.GameStoryline()'],['../d0/d7c/class_action_rpg_kit_1_1_tests_1_1_story_1_1_game_storyline.xhtml#ae727b573d523dd5d11afd002ebb81505',1,'ActionRpgKit::Tests::Story::GameStoryline.GameStoryline()']]],
   ['getcombatskillbyname',['GetCombatSkillByName',['../dd/dcc/class_action_rpg_kit_1_1_character_1_1_skill_1_1_skill_database.xhtml#a98a5130d84a236e0ac5c154abf7d8727',1,'ActionRpgKit::Character::Skill::SkillDatabase']]],
+  ['getitembyid',['GetItemById',['../dd/d83/class_action_rpg_kit_1_1_item_1_1_item_database.xhtml#af2ad9a07df33760dd27f2a44f3d1bf43',1,'ActionRpgKit::Item::ItemDatabase']]],
   ['getitembyname',['GetItemByName',['../dd/d83/class_action_rpg_kit_1_1_item_1_1_item_database.xhtml#ae500b4747c85689ee6004d5d9bc04a2d',1,'ActionRpgKit::Item::ItemDatabase']]],
   ['getmagicskillbyname',['GetMagicSkillByName',['../dd/dcc/class_action_rpg_kit_1_1_character_1_1_skill_1_1_skill_database.xhtml#af3d709e93dda41e266a77d322b2b9d3d',1,'ActionRpgKit::Character::Skill::SkillDatabase']]],
   ['getmodifier',['GetModifier',['../dc/dac/class_action_rpg_kit_1_1_character_1_1_skill_1_1_passive_magic_skill.xhtml#ab0bd75dac348340f9ff018b08a407c8f',1,'ActionRpgKit::Character::Skill::PassiveMagicSkill']]],
