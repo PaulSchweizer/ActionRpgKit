e6b0a4eaedd1fc2db5aa10a0e5b960862b0df49f
Paul Schweizer <paulschweizer@gmx.net>
2016-09-16 17:16:12 -0700

    Update StorylineImplementationTest.cs

--- ActionRpgKit/Tests/MainController/StorylineImplementationTest.cs
+++ ActionRpgKit/Tests/MainController/StorylineImplementationTest.cs
@@ -68,7 +68,7 @@ namespace ActionRpgKit.Tests
 
         public override void CheckProgress()
         {
-            int collectedHerbs = MainController.player.Inventory.GetQuantity(ItemDatabase.GetByName("Herb"));
+            int collectedHerbs = MainController.player.Inventory.GetQuantity(ItemDatabase.GetItemByName("Herb"));
             if (collectedHerbs == 10)
             {
                 IsCompleted = true;
