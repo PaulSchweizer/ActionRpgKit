1a98c335a62aedfe64195b9c294373e6096f2e02
Paul Schweizer <paulschweizer@gmx.net>
2016-09-23 10:17:14 -0700

    Update after_test.cmd

--- after_test.cmd
+++ after_test.cmd
@@ -2,6 +2,6 @@ nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
  
-"C:\projects\actionrpgkit\tools\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe" -target:C:\projects\actionrpgkit\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe "-targetargs:""ActionRpgKit/bin/Release/ActionRpgKit.dll"" /noshadow" -filter:"+[ActionRpgKit*]*" -register:user
+C:\projects\actionrpgkit\tools\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:C:\projects\actionrpgkit\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe "-targetargs:""C:/projects/actionrpgkit/ActionRpgKit/bin/Release/ActionRpgKit.dll"" /noshadow" -filter:"+[ActionRpgKit*]*" -register:user
 
-"C:\projects\actionrpgkit\tools\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe" --opencover -i .\results.xml 
+C:\projects\actionrpgkit\tools\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml 
