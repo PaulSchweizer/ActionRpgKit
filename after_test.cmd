abb029a7cb2129c9aa18ef411227f04df02fc911
Paul Schweizer <paulschweizer@gmx.net>
2016-09-23 10:30:54 -0700

    Update after_test.cmd

--- after_test.cmd
+++ after_test.cmd
@@ -4,4 +4,4 @@ nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
  
 .\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe -targetargs:"/nologo /noshadow .\ActionRpgKit\bin\Debug\ActionRpgKit.dll" -filter:"+[ActionRpgKit]*" -register:user
 
-.\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml 
+.\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE 
