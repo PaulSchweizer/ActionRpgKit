e67f24f024e9e3075e1a331242482a9a3c96853a
Paul Schweizer <paulschweizer@gmx.net>
2016-09-25 14:27:36 -0700

    test

--- after_test.cmd
+++ after_test.cmd
@@ -40,10 +40,10 @@ copy NUL .nojekyll
 rem Generate the Doxygen code documentation and log the output.
 echo 'Generating Doxygen code documentation...'
 rem Redirect both stderr and stdout to the log file AND the console.
-dir C:\projects\actionrpgkit\ActionRpgKit
+dir C:\projects\actionrpgkit\
 dir
 
-C:\projects\actionrpgkit\ActionRpgKit\tools\Doxygen.1.8.9.2\tools\doxygen.exe C:\projects\actionrpgkit\ActionRpgKit\config_appveyor.dox
+C:\projects\actionrpgkit\ActionRpgKit\tools\Doxygen.1.8.9.2\tools\doxygen.exe C:\projects\actionrpgkit\config_appveyor.dox
 
 xcopy C:\projects\actionrpgkit\doc\ActionRpgKit\html C:\projects\actionrpgkit\doc\ActionRpgKit /s /y
 
