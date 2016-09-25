e96d93b4a18907629b828c00f7227d410af37270
Paul Schweizer <paulschweizer@gmx.net>
2016-09-25 13:53:48 -0700

    test

--- after_test.cmd
+++ after_test.cmd
@@ -8,22 +8,38 @@ nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 nuget install Doxygen -Version 1.8.9.2 -OutputDirectory tools
 nuget install GraphViz.NET -OutputDirectory tools
 
+rem Create a clean working directory for this script.
 MD doc
 cd doc
 
+rem Get the current gh-pages branch
 git clone -b gh-pages https://git@github.com/PaulSchweizer/ActionRpgKit.git
 cd ActionRpgKit
 
+rem Configure git.
+rem Set the push default to simple i.e. push only the current branch.
 git config --global push.default simple
+rem Pretend to be an user called Travis CI.
 git config user.name "Appveyor"
 git config user.email "travis@travis-ci.org"
 
+rem Remove everything currently in the gh-pages branch.
+rem GitHub is smart enough to know which files have changed and which files have
+rem stayed the same and will only update the changed files. So the gh-pages branch
+rem can be safely cleaned, and it is sure that everything pushed later is the new
+rem documentation.
 del /q .\*
 for /d %%x in (.\*) do @rd /s /q "%%x"
 
+rem Need to create a .nojekyll file to allow filenames starting with an underscore
+rem to be seen on the gh-pages site. Therefore creating an empty .nojekyll file.
+rem Presumably this is only needed when the SHORT_NAMES option in Doxygen is set
+rem to NO, which it is by default. So creating the file just in case.
 copy NUL .nojekyll
 
+rem Generate the Doxygen code documentation and log the output.
 echo 'Generating Doxygen code documentation...'
+rem Redirect both stderr and stdout to the log file AND the console.
 C:\projects\actionrpgkit\ActionRpgKit\tools\Doxygen.1.8.9.2\tools\doxygen.exe config_appveyor.dox
 
 xcopy C:\projects\actionrpgkit\doc\ActionRpgKit\html C:\projects\actionrpgkit\doc\ActionRpgKit /s /y
@@ -37,5 +53,5 @@ git status
 git add --all
 
 git commit -m "Deploy code docs to GitHub Pages Appveyor build:" -m "Commit: "
-git push --force "https://${GH_REPO_TOKEN}@github.com/PaulSchweizer/ActionRpgKit.git"
+git push --force ("https://" + "$env:GH_REPO_TOKEN" + "@github.com/PaulSchweizer/ActionRpgKit.git")
 rem > /dev/null 2>&1
