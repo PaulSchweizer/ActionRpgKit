b56630f876dbbc342e29ef4faa07dd3ac72f022c
Paul Schweizer <paulschweizer@gmx.net>
2016-09-26 17:27:51 -0700

    Update after_test.cmd

--- after_test.cmd
+++ after_test.cmd
@@ -2,7 +2,7 @@ nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory tools
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
 
-.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe -targetargs:"/nologo /noshadow .\src\ActionRpgKit\bin\Debug\ActionRpgKit.NUnit.Tests.dll" -filter:"+[ActionRpgKit]*" -register:user
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe -targetargs:"/nologo /noshadow .\src\ActionRpgKit.NUnit.Tests\bin\Debug\ActionRpgKit.NUnit.Tests.dll" -filter:"+[ActionRpgKit]*" -register:user
 .\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml
 
 nuget install Doxygen -Version 1.8.9.2 -OutputDirectory tools
