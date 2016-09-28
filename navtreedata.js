f02bb3ffdd7087a117412d2329cfa9be3d83e6c0
Appveyor <appveyor@appveyor.com>
2016-09-28 03:40:36 +0000

    Deploy code docs to GitHub Pages Appveyor build:
    Commit:

--- navtreedata.js
+++ navtreedata.js
@@ -1,11 +1,32 @@
 var NAVTREE =
 [
-  [ "Action RPG Kit", "index.xhtml", ]
+  [ "Action RPG Kit", "index.xhtml", [
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.xhtml", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.xhtml", [
+        [ "All", "namespacemembers.xhtml", null ],
+        [ "Functions", "namespacemembers_func.xhtml", null ]
+      ] ]
+    ] ],
+    [ "Classes", null, [
+      [ "Class List", "annotated.xhtml", "annotated" ],
+      [ "Class Index", "classes.xhtml", null ],
+      [ "Class Hierarchy", "hierarchy.xhtml", "hierarchy" ],
+      [ "Class Members", "functions.xhtml", [
+        [ "All", "functions.xhtml", "functions_dup" ],
+        [ "Functions", "functions_func.xhtml", null ],
+        [ "Variables", "functions_vars.xhtml", null ],
+        [ "Properties", "functions_prop.xhtml", null ],
+        [ "Events", "functions_evnt.xhtml", null ]
+      ] ]
+    ] ]
+  ] ]
 ];
 
 var NAVTREEINDEX =
 [
-"index.xhtml"
+"annotated.xhtml",
+"da/d5b/class_action_rpg_kit_1_1_main_controller.xhtml#a5621a3f113ef5de5dfbb4c5d96a79e98"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
