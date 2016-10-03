74fded0c87b1f44c329a370ff50707655c75e4f5
Paul Schweizer <paulschweizer@gmx.net>
2016-10-03 12:41:19 -0700

    Create GameObjectTests.cs

---
+++ src/ActionRpgKit.NUnit.Tests/Core/GameObjectTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+using ActionRpgKit.Core;
+
+namespace ActionRpgKit.Tests.Core
+{
+
+    [TestFixture]
+    [Category("Core")]
+    public class GameObjectTests
+    {
+    }
+ }
