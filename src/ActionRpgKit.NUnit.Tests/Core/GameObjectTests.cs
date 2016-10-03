d638be5824220b5f3a3804b74e42bc1e26eb19d8
Paul Schweizer <paulschweizer@gmx.net>
2016-10-03 12:45:15 -0700

    Update GameObjectTests.cs

--- src/ActionRpgKit.NUnit.Tests/Core/GameObjectTests.cs
+++ src/ActionRpgKit.NUnit.Tests/Core/GameObjectTests.cs
@@ -4,10 +4,16 @@ using ActionRpgKit.Core;
 
 namespace ActionRpgKit.Tests.Core
 {
-
     [TestFixture]
     [Category("Core")]
     public class GameObjectTests
     {
+        GameObject gameObject;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            gameObject = new GameObject();
+        }
     }
  }
