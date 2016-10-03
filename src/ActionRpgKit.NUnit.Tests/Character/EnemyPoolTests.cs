51ea72054dde4f13abcd1622d754df943354e536
Paul Schweizer <paulschweizer@gmx.net>
2016-10-03 12:43:51 -0700

    Create EnemyPoolTests.cs

---
+++ src/ActionRpgKit.NUnit.Tests/Character/EnemyPoolTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using ActionRpgKit.Character;
+
+namespace ActionRpgKit.Tests.Character
+{
+    
+    [TestFixture]
+    [Category("Character.Character")]
+    class CharacterTests
+    {
+        EnemyPool enemyPool;
+
+        [SetUp]
+        public void SetUp()
+        {
+            enemyPool = new EnemyPool();
+            GameTime.Reset();
+        }
+    }
+}
