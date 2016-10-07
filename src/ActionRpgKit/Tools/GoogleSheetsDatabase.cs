8344dac552890d818f28adb2de5cb5161da5f4a6
Paul Schweizer <paulschweizer@gmx.net>
2016-10-07 14:30:28 -0700

    Update GoogleSheetsDatabase.cs

--- src/ActionRpgKit/Tools/GoogleSheetsDatabase.cs
+++ src/ActionRpgKit/Tools/GoogleSheetsDatabase.cs
@@ -1,47 +1,47 @@
-using Google.GData.Client;
-using Google.GData.Extensions;
-using Google.GData.Spreadsheets;
+// using Google.GData.Client;
+// using Google.GData.Extensions;
+// using Google.GData.Spreadsheets;
 
-namespace ActionRpgKit.Tools
-{
-    public class GoogleSheetsDatabase
-    {
+// namespace ActionRpgKit.Tools
+// {
+//     public class GoogleSheetsDatabase
+//     {
 
-        public GoogleSheetsDatabase ()
-        {
-            SpreadsheetsService myService = new SpreadsheetsService("exampleCo-exampleApp-1");
-            myService.setUserCredentials("schweizerpau@gmail.com", "mypassword");
+//         public GoogleSheetsDatabase ()
+//         {
+//             SpreadsheetsService myService = new SpreadsheetsService("exampleCo-exampleApp-1");
+//             myService.setUserCredentials("schweizerpau@gmail.com", "mypassword");
 
-            SpreadsheetQuery query = new SpreadsheetQuery();
-            SpreadsheetFeed feed = myService.Query(query);
+//             SpreadsheetQuery query = new SpreadsheetQuery();
+//             SpreadsheetFeed feed = myService.Query(query);
 
-            Console.WriteLine("Your spreadsheets: ");
-            foreach (SpreadsheetEntry entry in feed.Entries)
-            {
-                Console.WriteLine(entry.Title.Text);
-            }
+//             Console.WriteLine("Your spreadsheets: ");
+//             foreach (SpreadsheetEntry entry in feed.Entries)
+//             {
+//                 Console.WriteLine(entry.Title.Text);
+//             }
 
-            AtomLink link = entry.Links.FindService(GDataSpreadsheetsNameTable.WorksheetRel, null);
+//             AtomLink link = entry.Links.FindService(GDataSpreadsheetsNameTable.WorksheetRel, null);
 
-            WorksheetQuery query = new WorksheetQuery(link.HRef.ToString());
-            WorksheetFeed feed = service.Query(query);
+//             WorksheetQuery query = new WorksheetQuery(link.HRef.ToString());
+//             WorksheetFeed feed = service.Query(query);
 
-            foreach (WorksheetEntry worksheet in feed.Entries)
-            {
-                Console.WriteLine(worksheet.Title.Text);
-            }
+//             foreach (WorksheetEntry worksheet in feed.Entries)
+//             {
+//                 Console.WriteLine(worksheet.Title.Text);
+//             }
 
-            AtomLink cellFeedLink = worksheetentry.Links.FindService(GDataSpreadsheetsNameTable.CellRel, null);
+//             AtomLink cellFeedLink = worksheetentry.Links.FindService(GDataSpreadsheetsNameTable.CellRel, null);
 
-            CellQuery query = new CellQuery(cellFeedLink.HRef.ToString());
-            CellFeed feed = service.Query(query);
+//             CellQuery query = new CellQuery(cellFeedLink.HRef.ToString());
+//             CellFeed feed = service.Query(query);
 
-            Console.WriteLine("Cells in this worksheet:");
-            foreach (CellEntry curCell in feed.Entries)
-            {
-                Console.WriteLine("Row {0}, column {1}: {2}", curCell.Cell.Row,
-                    curCell.Cell.Column, curCell.Cell.Value);
-            }
-        }
-    }
-}
+//             Console.WriteLine("Cells in this worksheet:");
+//             foreach (CellEntry curCell in feed.Entries)
+//             {
+//                 Console.WriteLine("Row {0}, column {1}: {2}", curCell.Cell.Row,
+//                     curCell.Cell.Column, curCell.Cell.Value);
+//             }
+//         }
+//     }
+// }
