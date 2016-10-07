d75127e5c3567b796e28a892845b5de062576232
Paul Schweizer <paulschweizer@gmx.net>
2016-10-07 11:31:13 -0700

    Create EnemyDatabase.cs

---
+++ src/ActionRpgKit/Character/EnemyDatabase.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+using ActionRpgKit.Character.Stats;
+
+namespace ActionRpgKit.Character
+{
+    /// <summary>
+    /// Holds all available enemy types.</summary>
+    public class EnemyDatabase
+    {
+    
+        /// <summary>
+        /// The internal List of Enemie types.</summary>
+        private EnemyStats[] EnemyStats { get; set; }
+
+    }
+}
