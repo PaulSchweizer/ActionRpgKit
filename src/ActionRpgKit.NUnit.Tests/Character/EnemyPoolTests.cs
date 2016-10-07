b12eb2e984b779484b4a071c7854d41df9318767
Paul Schweizer <paulschweizer@gmx.net>
2016-10-07 11:21:36 -0700

    Update EnemyPoolTests.cs

--- src/ActionRpgKit.NUnit.Tests/Character/EnemyPoolTests.cs
+++ src/ActionRpgKit.NUnit.Tests/Character/EnemyPoolTests.cs
@@ -10,24 +10,31 @@ namespace ActionRpgKit.Tests.Character
     [Category("Character.Character")]
     class EnemyPoolTests
     {
-        EnemyPool enemyPool = new EnemyPool(size: 1);
+        EnemyPool enemyPool;
 
         [SetUp]
         public void SetUp()
         {
-//             enemyPool = new EnemyPool(size: 1);
+            enemyPool = new EnemyPool(size: 1);
             GameTime.Reset();
         }
         
         [Test]
         public void TestSize()
         {
+            // Retrieve an enemy and release it back to the pool
+            var enemy = enemyPool.Acquire();
+            enemyPool.Release(enemy);
+            enemyPool.Acquire();
+            Assert.AreEqual(1, enemyPool.Size);
+            
             // Retrieve more enemies than the pool has to offer, will increase the size.
             for(int i=0; i < 2; i++)
             {
                 enemyPool.Acquire();
             }
             Assert.AreEqual(2, enemyPool.Size);
+            
         }
     }
 }
