b053b92c8713c28e31e18a508d50a6489de0404b
Paul Schweizer <paulschweizer@gmx.net>
2016-10-12 15:15:41 -0700

    Create Projectile.cs

---
+++ src/ActionRpgKit/Character/Projectile.cs
@@ -0,0 +1,16 @@
+using System;
+using ActionRpgKit.Core;
+
+namespace ActionRpgKit.Character
+{
+
+    /// <summary>
+    /// A projectile moving at certain speed in a certain direction.
+    /// On hitting, the projectile is being destroyed and damage is applied to the target.</summary>
+    public class Projectile : IGameObject
+    {
+        
+    
+    }
+   
+}
