5c15c9cdafe430009781081b1afe0825c4485123
Paul Schweizer <paulschweizer@gmx.net>
2016-10-12 15:31:11 -0700

    Update Projectile.cs

--- src/ActionRpgKit/Character/Projectile.cs
+++ src/ActionRpgKit/Character/Projectile.cs
@@ -9,8 +9,19 @@ namespace ActionRpgKit.Character
     /// On hitting, the projectile is being destroyed and damage is applied to the target.</summary>
     public class Projectile : IGameObject
     {
+        public float Speed;
+        public Position Destination;
+        public float Damage;
+        public Position Position { get; set; }
         
-    
+        public void OnDestinationReached ()
+        {
+        
+        }
+        
+        public void OnCharacterHit (BaseCharacter character)
+        {
+        
+        }
     }
-   
 }
